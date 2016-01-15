3a4908d46881391dd33b3ba1bd963415e49e101e
okoala <dapixp@gmail.com>
2016-01-15 14:02:34 +0800

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var compiler = require('vueify').compiler;
 
 module.exports = function (content, file, conf) {
-    var result = compiler.compile(content, function(err){
+    var result = compiler.compile(content.toString('utf8'), file.realpath, function(err, result){
         if (err) {
             console.log('fis3-parser-vue Error: ');
             console.log(err);
